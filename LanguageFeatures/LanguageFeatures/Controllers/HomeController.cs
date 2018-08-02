2d193bbcd098c5b0c0b00a9444374548d61a9191
Adam Deng <adamdeng@gmail.com>
2018-08-02 09:41:30 -0700

    c# features

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                string category = p?.Category ?? "<None>";
+                results.Add($"Name:{name},Price:{price},Related:{relatedName}");
+            }
+            return View(results);
+        }
+    }
+}
